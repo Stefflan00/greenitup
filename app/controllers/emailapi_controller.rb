be5cec122999c8d13b325f1c4c8eb33469df6071
Stefflan00 <stefan.maier09@gmail.com>
2014-10-11 19:25:36 +0200

    falsh errors

--- app/controllers/emailapi_controller.rb
+++ app/controllers/emailapi_controller.rb
@@ -8,14 +8,17 @@ def subscribe
 
   @list_id = "b7146ccd68"
     gb = Gibbon::API.new
+  begin
+      gb.lists.subscribe({
+        :id => @list_id,
+        :email => {:email => params[:email][:address]}
+        })
+        #redirect_to :back
+        flash[:notice] = "You successfully subscribed"
 
-    gb.lists.subscribe({
-      :id => @list_id,
-      :email => {:email => params[:email][:address]}
-      })
-      redirect_to :back
-      flash[:notice] = "You successfully subscribed"
-
+  rescue Gibbon::MailChimpError => e
+    flash[:error] = "Sorry there was an error while subscribing: #{e.message} :("
+  end
 
 end
 
--- config/initializers/gibbon.rb
+++ config/initializers/gibbon.rb
@@ -1,3 +1,3 @@
-Gibbon::API.api_key = "943882f3ccfb7a399fdb61a6b5e4e594-us9"
+Gibbon::API.api_key = "87ce70e33499e8f1e82266115786d9a6-us9"
 Gibbon::API.timeout = 15
 Gibbon::API.throws_exceptions = true
